dbf895a9a608b9a40b33e5ddedd27872edc9a5fa
tmaj2022 <110788688+tmaj2022@users.noreply.github.com>
2022-08-07 17:05:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's new healthy diner')
+streamlit.title('My Mom''s new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
