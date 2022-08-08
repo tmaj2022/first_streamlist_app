af13a462093ac3c21a2f1b9aea8660f0a7150c94
tmaj2022 <110788688+tmaj2022@users.noreply.github.com>
2022-08-08 17:56:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
 #streamlit.stop()
 
 def get_fruit_load_list():
-  with my_cnx.cursor() as mu_cur;
+  with my_cnx.cursor() as my_cur;
        my_cur.execute("SELECT * from fruit_load_list")
        return my_cur.fetchall()
 
