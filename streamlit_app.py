c8300446672550d1933b54b884a46e66b39ed98b
tmaj2022 <110788688+tmaj2022@users.noreply.github.com>
2022-08-08 15:30:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLerror
+from urllib.error import URLRrror
 
 
 streamlit.title('My Mom''s new healthy diner')
