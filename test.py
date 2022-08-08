0907ba07ff78aa83342a5bff5a58fff6b610e4c1
tmaj2022 <110788688+tmaj2022@users.noreply.github.com>
2022-08-08 17:32:20 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+#from urllib.error import URLError
+
+
+streamlit.title('My Mom''s new healthy diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
