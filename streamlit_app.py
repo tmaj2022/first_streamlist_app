cc60171e464a43bfd9fc901522a4265134950549
tmaj2022 <110788688+tmaj2022@users.noreply.github.com>
2022-08-08 15:54:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,4 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-#from urllib.error import URLError
 
-
-streamlit.title('My Mom''s new healthy diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
 
 import streamlit
 import pandas
